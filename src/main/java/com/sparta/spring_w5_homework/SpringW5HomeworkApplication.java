ec2a08ac523370870975faa16b31a65b6565a93e
guluming <guluming@naver.com>
2022-06-03 14:16:44 +0900

    기본 설정 파일

---
+++ src/main/java/com/sparta/spring_w5_homework/SpringW5HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_w5_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringW5HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringW5HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/spring_w5_homework/SpringW5HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_w5_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringW5HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
