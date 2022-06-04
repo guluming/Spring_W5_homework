0341dcdf4bd1135d0bfa1b38db467082ce8ce909
guluming <guluming@naver.com>
2022-06-04 20:13:37 +0900

    list형태로 넘어온 json 데이터를 for문을 통해서 조건검사후 db에 삽입
    하는 로직으로 변경

--- src/main/java/com/sparta/spring_w5_homework/service/ResFoodService.java
+++ src/main/java/com/sparta/spring_w5_homework/service/ResFoodService.java
@@ -1,10 +1,9 @@
 package com.sparta.spring_w5_homework.service;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.spring_w5_homework.model.ResFood;
 import com.sparta.spring_w5_homework.repository.ResFoodRepository;
 import com.sparta.spring_w5_homework.requestdto.ResFoodRequestDto;
+import com.sparta.spring_w5_homework.requestdto.ResFoodRequestListDto;
 import com.sparta.spring_w5_homework.responsedto.ResFoodResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,44 +22,35 @@ public class ResFoodService {
 
     //음식 등록
     @Transactional
-    public String foodSave(Long restaurantId, ResFoodRequestDto resFoods){
+    public String foodSave(Long restaurantId, List<ResFoodRequestListDto> resFoodDtoList){
         LocalDateTime modifiedAt = LocalDateTime.now();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        String jsonSting = objectMapper.writeValueAsString(resFoods);
-        DTO Dtos[] = objectMapper.readValue(jsonSting,Event[].class);
-
-        List<ResFoodRequestDto.Foods> foods = resFoods.getFoods();
         if(restaurantId == 0){
             return "음식점 ID를 입력해주세요.";
         }
 
-        for(int i = 0; i < foods.size(); i++){
-            food = foods.get(i);
+        for(int i = 0; i < resFoodDtoList.size(); i++){
 
-            if(food.size() == 0){
-                return "등록하실 음식을 적어주세요.";
-            }
-
-            String name = food.get(0);
-            int price = Integer.parseInt(food.get(1));
-
-            Optional<ResFood> found = resFoodRepository.findByRestaurantIdAndName(restaurantId, name);
+            Optional<ResFood> found = resFoodRepository.findByRestaurantIdAndName(restaurantId, resFoodDtoList.get(i).getName());
 
             if(found.isPresent()){
                 return "이미 음식이 존재합니다.";
             }
 
-            if(price < 100 && price > 1000000){
+            if(resFoodDtoList.get(i).getPrice() > 100 && resFoodDtoList.get(i).getPrice() < 1000000){
+
+                ResFoodRequestDto params = new ResFoodRequestDto(resFoodDtoList.get(i).getName(), resFoodDtoList.get(i).getPrice(), restaurantId, modifiedAt);
+
+                ResFood resFood = new ResFood(params);
+                resFoodRepository.save(resFood);
+
+            } else {
                 return "음식 가격의 허용 범위는 100원 ~ 1,000,000원 입니다.";
-            } else if (price%100 != 0) {
-                return "음식 가격은 100원 단위로만 입력 가능합니다.";
             }
 
-            ResFood resFood = new ResFood(name, price, restaurantId, modifiedAt);
-            resFoodRepository.save(resFood);
+            if (resFoodDtoList.get(i).getPrice()%100 != 0) {
+                return "음식 가격은 100원 단위로만 입력 가능합니다.";
+            }
         }
         return "음식이 등록 되었습니다.";
     }
