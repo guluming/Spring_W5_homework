11ff13ebc7b8248fb6641b90dc8cfe91fca5092d
guluming <guluming@naver.com>
2022-06-04 20:10:11 +0900

    생성일 수정일 변수 삭제

--- src/main/java/com/sparta/spring_w5_homework/responsedto/ResFoodResponseDto.java
+++ src/main/java/com/sparta/spring_w5_homework/responsedto/ResFoodResponseDto.java
@@ -4,22 +4,16 @@ import com.sparta.spring_w5_homework.model.ResFood;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @NoArgsConstructor
 @Getter
 public class ResFoodResponseDto {
     private Long id;
     private String name;
     private int price;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
 
     public ResFoodResponseDto(ResFood entity){
         this.id = entity.getId();
         this.name = entity.getName();
         this.price = entity.getPrice();
-        this.createdAt = entity.getCreatedAt();
-        this.modifiedAt = entity.getModifiedAt();
     }
 }
