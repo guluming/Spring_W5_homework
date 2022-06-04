52f86f58b00d31eac8a1fae9136815360e3d1d0c
guluming <guluming@naver.com>
2022-06-04 20:09:15 +0900

    Dto를 통해서 데이터를 받는거로 수정

--- src/main/java/com/sparta/spring_w5_homework/model/ResFood.java
+++ src/main/java/com/sparta/spring_w5_homework/model/ResFood.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
@@ -32,19 +31,12 @@ public class ResFood extends Timestamped {
     @LastModifiedDate
     private LocalDateTime modifiedAt = LocalDateTime.now();
 
-    public ResFood(String name, int price, Long restaurantId, LocalDateTime modifiedAt){
-        this.name = name;
-        this.price = price;
-        this.restaurantId = restaurantId;
-        this.modifiedAt = modifiedAt;
+    public ResFood(ResFoodRequestDto params){
+        this.name = params.getName();
+        this.price = params.getPrice();
+        this.restaurantId = params.getRestaurantId();
+        this.modifiedAt = params.getModifiedAt();
     }
-
-//    public ResFood(ResFoodRequestDto params, Long restaurantId){
-//        this.name = params.getFoods().get(1)
-//        this.price = params.getFoods();
-//        this.modifiedAt = params.getModifiedAt();
-//        this.restaurantId = restaurantId;
-//    }
 
     public void update(ResFoodRequestDto params){
 
