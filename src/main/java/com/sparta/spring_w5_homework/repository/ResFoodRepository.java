cb21cd3eeeb6f0342fc10bb1e3ea4060e71d614a
guluming <guluming@naver.com>
2022-06-04 09:23:57 +0900

    Food repository 이름 변경

---
+++ src/main/java/com/sparta/spring_w5_homework/repository/ResFoodRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.spring_w5_homework.repository;
+
+import com.sparta.spring_w5_homework.model.ResFood;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ResFoodRepository extends JpaRepository<ResFood, Long> {
+    List<ResFood> findByrestaurantId(Long restaurantId);
+//    Optional<ResFood> findAllByResfood(Long restaurantId);
+    Optional<ResFood> findByRestaurantIdAndName(Long restaurantId, String name);
+    Optional<ResFood> findByRestaurantIdAndId(Long restaurantId, Long Id);
+}
