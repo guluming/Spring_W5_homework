4392e03e84e78079810b24cebbb0b308a363ab51
guluming <guluming@naver.com>
2022-06-04 20:09:34 +0900

    ObjectMapper objectMapper 를 사용하기 위한 Dto

---
+++ src/main/java/com/sparta/spring_w5_homework/requestdto/ResFoodRequestListDto.java
@@ -0,0 +1,11 @@
+package com.sparta.spring_w5_homework.requestdto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ResFoodRequestListDto {
+    private String name;
+    private int price;
+}
