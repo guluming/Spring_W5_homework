6bace0b81dd21299ef058186c8afa8a6567a2ffb
guluming <guluming@naver.com>
2022-06-04 09:22:36 +0900

    알림설정

--- src/main/java/com/sparta/spring_w5_homework/SpringW5HomeworkApplication.java
+++ src/main/java/com/sparta/spring_w5_homework/SpringW5HomeworkApplication.java
@@ -2,7 +2,9 @@ package com.sparta.spring_w5_homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringW5HomeworkApplication {
 
