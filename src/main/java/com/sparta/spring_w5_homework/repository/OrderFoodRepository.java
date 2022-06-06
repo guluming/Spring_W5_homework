3e4609f7a229faab9d254eac382ba43972f0075a
guluming <guluming@naver.com>
2022-06-07 00:10:36 +0900

    OrderId로 repo 검색 구현

--- src/main/java/com/sparta/spring_w5_homework/repository/OrderFoodRepository.java
+++ src/main/java/com/sparta/spring_w5_homework/repository/OrderFoodRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.spring_w5_homework.repository;
 
 import com.sparta.spring_w5_homework.model.OrderFood;
+import com.sparta.spring_w5_homework.repository.mapping.OrderFoodInfoMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderFoodRepository extends JpaRepository<OrderFood, Long> {
+    List<OrderFood> findAllByordersId(Long ordersId);
+//    List<OrderFoodInfoMapping> findAll();
 }
