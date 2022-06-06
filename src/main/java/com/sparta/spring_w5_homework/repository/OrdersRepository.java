a7f4537cb9d7c95b9e3d7661e0ccfdc33b182443
guluming <guluming@naver.com>
2022-06-06 11:15:15 +0900

    코드 정리

--- src/main/java/com/sparta/spring_w5_homework/repository/OrdersRepository.java
+++ src/main/java/com/sparta/spring_w5_homework/repository/OrdersRepository.java
@@ -3,9 +3,5 @@ package com.sparta.spring_w5_homework.repository;
 import com.sparta.spring_w5_homework.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
-    Optional<Orders> findBymodifiedAt(LocalDateTime modifiedAt);
 }
