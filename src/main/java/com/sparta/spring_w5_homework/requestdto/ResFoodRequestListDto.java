9907e8066b611e52605ed534d2577c8ea7f4a660
guluming <guluming@naver.com>
2022-06-07 17:18:12 +0900

    음식등록 컨트롤러 데이터 받는 로직 변경에 따른 코드 수정

--- src/main/java/com/sparta/spring_w5_homework/requestdto/ResFoodRequestListDto.java
+++ src/main/java/com/sparta/spring_w5_homework/requestdto/ResFoodRequestListDto.java
@@ -1,11 +1,15 @@
 package com.sparta.spring_w5_homework.requestdto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 public class ResFoodRequestListDto {
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("price")
     private int price;
 }
