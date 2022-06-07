e868a0d8cc220f42e7a72b8d700d2fca88350e18
guluming <guluming@naver.com>
2022-06-07 17:19:42 +0900

    기존에 키값으로 데이터를 받는 방식에서 Json형식으로 한번에 받는 방법으로 변경 / 그에 따른 서비스 로직 또한 변경

--- src/main/java/com/sparta/spring_w5_homework/controller/OrdersController.java
+++ src/main/java/com/sparta/spring_w5_homework/controller/OrdersController.java
@@ -1,15 +1,11 @@
 package com.sparta.spring_w5_homework.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.spring_w5_homework.model.Orders;
-import com.sparta.spring_w5_homework.requestdto.OrderFoodsListDto;
-import com.sparta.spring_w5_homework.responsedto.FoodOrderResponseDto;
+import com.sparta.spring_w5_homework.requestdto.OrderListDto;
 import com.sparta.spring_w5_homework.responsedto.OrdersResponseDto;
 import com.sparta.spring_w5_homework.service.OrdersService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,23 +19,13 @@ public class OrdersController {
 
     //주문
     @PostMapping("/order/request")
-    public OrdersResponseDto save(@RequestParam("restaurantId") Long restaurantId, @RequestParam("foods") String jsonList) throws JsonProcessingException {
-
-        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        List<OrderFoodsListDto> orderFoodsDtoList = objectMapper.readValue(jsonList, new TypeReference<List<OrderFoodsListDto>>() {});
-
-        return ordersService.ordersSave(restaurantId ,orderFoodsDtoList);
+    public OrdersResponseDto save(@RequestBody OrderListDto params) {
+        return ordersService.ordersSave(params.getRestaurantId(), params.getFoods());
     }
 
     //주문 조회
     @GetMapping("/orders")
-    public List<OrdersResponseDto> findAll(){
+    public List<OrdersResponseDto> findAll() {
         return ordersService.ordersFindAll();
     }
-
-    //주문 조회
-//    @GetMapping("/orders")
-//    public List<Orders> findAll(){
-//        return ordersService.ordersFindAll();
-//    }
 }
--- src/main/java/com/sparta/spring_w5_homework/service/OrdersService.java
+++ src/main/java/com/sparta/spring_w5_homework/service/OrdersService.java
@@ -4,23 +4,26 @@ import com.sparta.spring_w5_homework.model.OrderFood;
 import com.sparta.spring_w5_homework.model.ResFood;
 import com.sparta.spring_w5_homework.model.Orders;
 import com.sparta.spring_w5_homework.model.Restaurant;
+
 import com.sparta.spring_w5_homework.repository.OrderFoodRepository;
 import com.sparta.spring_w5_homework.repository.ResFoodRepository;
 import com.sparta.spring_w5_homework.repository.OrdersRepository;
 import com.sparta.spring_w5_homework.repository.RestaurantRepository;
-import com.sparta.spring_w5_homework.requestdto.OrderFoodsListDto;
+
 import com.sparta.spring_w5_homework.requestdto.OrdersRequestDto;
+
 import com.sparta.spring_w5_homework.responsedto.FoodOrderResponseDto;
 import com.sparta.spring_w5_homework.responsedto.OrdersResponseDto;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -30,31 +33,30 @@ public class OrdersService {
     private final ResFoodRepository resFoodRepository;
     private final OrderFoodRepository orderFoodRepository;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //주문
     @Transactional
-    public OrdersResponseDto ordersSave(Long restaurantId, List<OrderFoodsListDto> orderFoodsDtoList) {
-//        LocalDateTime modifiedAt = LocalDateTime.now();
-        if(restaurantId == 0L){
+    public OrdersResponseDto ordersSave(Long restaurantId, ArrayList<Map<String,Object>> orderfoods) {
+        if (restaurantId == 0) {
             throw new IllegalArgumentException("해당 음식점을 찾을 수 없습니다.");
 //            return "해당 음식점을 찾을 수 없습니다.";
         }
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new NullPointerException("음식점이 존재 하지 않습니다."));
 
+        //쓰이는 곳이 한 곳이면 선언하지말고 바로 넣기
         String restaurantName = restaurant.getName();
         int deliveryFee = restaurant.getDeliveryFee();
         int minOrderPrice = restaurant.getMinOrderPrice();
         int totalPrice = 0;
 
-        for(int i=0; i<orderFoodsDtoList.size(); i++) {
+        for (int i = 0; i < orderfoods.size(); i++) {
 
-            Long foodId = orderFoodsDtoList.get(i).getFoodId();
-            int quantity = orderFoodsDtoList.get(i).getQuantity();
+            Long foodId = ((Number) orderfoods.get(i).get("id")).longValue();
+            int quantity = (Integer) orderfoods.get(i).get("quantity");
 
             ResFood orderResFood = resFoodRepository.findByRestaurantIdAndId(restaurantId, foodId).orElseThrow(() -> new NullPointerException("해당 음식은 해당 음식점에 없습니다."));
 
-            if(1 <= quantity && quantity <= 100){
+            if (1 <= quantity && quantity <= 100) {
 
                 int price = quantity * orderResFood.getPrice();
 
@@ -64,7 +66,7 @@ public class OrdersService {
             }
         }
 
-        if(totalPrice < minOrderPrice){
+        if (totalPrice < minOrderPrice) {
             throw new IllegalArgumentException("최소 주문 가격을 확인해 주세요.");
         }
 
@@ -77,10 +79,10 @@ public class OrdersService {
 
         List<OrderFood> foods = new ArrayList<>();
 
-        for(int i=0; i<orderFoodsDtoList.size(); i++) {
+        for (int i = 0; i < orderfoods.size(); i++) {
 
-            Long foodId = orderFoodsDtoList.get(i).getFoodId();
-            int quantity = orderFoodsDtoList.get(i).getQuantity();
+            Long foodId = ((Number) orderfoods.get(i).get("id")).longValue();
+            int quantity = (Integer) orderfoods.get(i).get("quantity");
 
             ResFood orderResFood = resFoodRepository.findByRestaurantIdAndId(restaurantId, foodId).orElseThrow(() -> new NullPointerException("해당 음식은 해당 음식점에 없습니다."));
 
@@ -96,7 +98,7 @@ public class OrdersService {
 
         List<FoodOrderResponseDto> foodOrderResponseDtos = new ArrayList<>();
 
-        for(int i=0; i<foods.size(); i++){
+        for (int i = 0; i < foods.size(); i++) {
             String foodName = foods.get(i).getFoodName();
             int quantity = foods.get(i).getQuantity();
             int price = foods.get(i).getPrice();
@@ -108,41 +110,35 @@ public class OrdersService {
         }
         OrdersResponseDto ordersResponseDto = new OrdersResponseDto(ordersId.getRestaurantName(), foodOrderResponseDtos, ordersId.getDeliveryFee(), ordersId.getTotalPrice());
 
-//        System.out.println(foodOrderResponseDtos.get(0).getFoodName().toString());
-
         return ordersResponseDto;
     }
 
     //주문 전체 조회
-    public List<OrdersResponseDto> ordersFindAll(){
-//        Sort sort = Sort.by(Sort.Direction.ASC, "id");
+    public List<OrdersResponseDto> ordersFindAll() {
+        Sort sort = Sort.by(Sort.Direction.ASC, "id");
         List<OrdersResponseDto> ordersResponseDtos = new ArrayList<>();
         List<FoodOrderResponseDto> foodOrderResponseDtos = new ArrayList<>();
-        List<Orders> orderslist = ordersRepository.findAll();
+        List<Orders> orderList = ordersRepository.findAll(sort);
 
-        for(int i=0; i<orderslist.size(); i++){
+        for (int i = 0; i < orderList.size(); i++) {
 
-            List<OrderFood> orderFoodslist = orderFoodRepository.findAllByordersId(orderslist.get(i).getId());
+            List<OrderFood> orderFoodList = orderFoodRepository.findAllByordersId(orderList.get(i));
 
-            for(int j=0; j<orderFoodslist.size(); j++){
+            for (int j = 0; j < orderFoodList.size(); j++) {
 
-                String foodname = orderFoodslist.get(j).getFoodName();
-                int quantity = orderFoodslist.get(j).getQuantity();
-                int price = orderFoodslist.get(j).getPrice();
+                String foodName = orderFoodList.get(j).getFoodName();
+                int quantity = orderFoodList.get(j).getQuantity();
+                int price = orderFoodList.get(j).getPrice();
 
-                FoodOrderResponseDto foodOrderResponseDto = new FoodOrderResponseDto(foodname, quantity, price);
+                FoodOrderResponseDto foodOrderResponseDto = new FoodOrderResponseDto(foodName, quantity, price);
                 foodOrderResponseDtos.add(foodOrderResponseDto);
             }
+
+            OrdersResponseDto ordersResponseDto = new OrdersResponseDto(orderList.get(i).getRestaurantName(), foodOrderResponseDtos,
+                    orderList.get(i).getDeliveryFee(), orderList.get(i).getTotalPrice());
+
+            ordersResponseDtos.add(ordersResponseDto);
         }
-
-        
-
         return ordersResponseDtos;
     }
-
-    //주문 전체 조회
-//    public List<Orders> ordersFindAll(){
-//        Sort sort = Sort.by(Sort.Direction.ASC, "id");
-//        return ordersRepository.findAll(sort);
-//    }
 }
